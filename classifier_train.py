7e3a905b6765454df9f2749f5a5c213a202ac46a
abonin <abonincpge@gmail.com>
2024-10-11 11:30:19 +0200

    Different head architecture

--- classifier_train.py
+++ classifier_train.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR, ReduceLROnPlateau
 from torchvision.transforms import v2
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms, models
@@ -24,19 +25,7 @@ def set_seed(seed=31415):
     torch.manual_seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-def setup_matplotlib():
-    """Set up Matplotlib defaults."""
-    plt.style.use('ggplot')
-    plt.rc('figure', autolayout=True)
-    plt.rc('axes', labelweight='bold', labelsize='large',
-           titleweight='bold', titlesize=18, titlepad=10)
-    plt.rc('animation', html='html5')
-    plt.rc('image', cmap='magma')
-    rcParams['figure.figsize'] = (18, 8)
-    rcParams['axes.spines.top'] = False
-    rcParams['axes.spines.right'] = False
-
-def create_data_generators(datapath, img_size, batch_size=25, val_split=0.2):
+def create_data_generators(datapath, val_path = None, batch_size=25, val_split=0.2):
     """Create training and validation data generators."""
     transform = transforms.Compose([
         v2.RandomResizedCrop(size=(224, 224), antialias=True),
@@ -48,65 +37,82 @@ def create_data_generators(datapath, img_size, batch_size=25, val_split=0.2):
     # Load the entire dataset
     full_dataset = datasets.ImageFolder(root=datapath, transform=transform)
     
-    # Calculate the number of samples for training and validation
-    val_size = int(len(full_dataset) * val_split)
-    train_size = len(full_dataset) - val_size
-    
-    # Split the dataset
-    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
-    
+    if val_path==None: 
+        # Calculate the number of samples for training and validation
+        val_size = int(len(full_dataset) * val_split)
+        train_size = len(full_dataset) - val_size
+        
+        # Split the dataset
+        train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    else:  
+        train_dataset = full_dataset
+        val_dataset = datasets.ImageFolder(root=val_path, transform=transform)
+        print(f'Using validation set from {val_path}')
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     
     return train_loader, val_loader
 
-def create_model(img_size):
+def create_model(model_path=None):
     """Create and compile the model."""
-    model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
-    num_ftrs = model.classifier[1].in_features
-    model.classifier = nn.Sequential(
-        nn.Linear(num_ftrs, 1024),
-        nn.ReLU(),
-        nn.Linear(1024, 3),
-        nn.Softmax(dim=1)
-    )
-    
+    if model_path:
+        # Load the model architecture
+        model = models.efficientnet_b0(weights=None)
+        
+        num_ftrs = model.classifier[1].in_features
+
+        model.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, 512),          # Linear bottleneck to reduce dimensionality
+            nn.SiLU(),                         # Swish activation function (SiLU is the PyTorch equivalent)
+            nn.LayerNorm(512),                 # Layer normalization for stability
+            nn.Dropout(p=0.4),                 # Regularization (adjust dropout rate if necessary)
+            nn.Linear(512, 256),               # Another dense layer with reduced size for better feature extraction
+            nn.SiLU(),                         # Activation again for non-linearity
+            nn.LayerNorm(256),                 # Layer normalization after each dense layer
+            nn.Dropout(p=0.3),                 # Dropout applied again for robustness
+            nn.Linear(256, 3),                 # Final classification layer
+        )
+
+        # Load the pretrained weights
+        model.load_state_dict(torch.load(model_path))
+    else:
+        # Create a new model with or without pretrained weights
+        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
+        
+        # Modify the classifier
+        num_ftrs = model.classifier[1].in_features
+
+        model.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, 512),          # Linear bottleneck to reduce dimensionality
+            nn.SiLU(),                         # Swish activation function (SiLU is the PyTorch equivalent)
+            nn.LayerNorm(512),                 # Layer normalization for stability
+            nn.Dropout(p=0.4),                 # Regularization (adjust dropout rate if necessary)
+            nn.Linear(512, 256),               # Another dense layer with reduced size for better feature extraction
+            nn.SiLU(),                         # Activation again for non-linearity
+            nn.LayerNorm(256),                 # Layer normalization after each dense layer
+            nn.Dropout(p=0.3),                 # Dropout applied again for robustness
+            nn.Linear(256, 3),                 # Final classification layer
+        )
+
     return model
 
-def get_callbacks(savepath):
+def get_callbacks(patience):
     """Create callbacks for training."""
     early_stopping = {
-        'patience': 10,
-        'min_delta': 0.001,
+        'patience': patience,
         'counter': 0,
         'best_loss': None,
         'early_stop': False
     }
 
-    checkpoint_path = os.path.join(savepath, f'{run_name}.pth')
+    return early_stopping
 
-    return early_stopping, checkpoint_path
-
-def plot_history(history_df, save=True):
-    """Plot training and validation loss."""
-    history_df.loc[:, ['loss', 'val_loss']].plot()
-    plt.title('Training and Validation Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-
-    if save:
-        save_path = os.path.join(graphs_folder, f'{run_name}.png')
-        plt.savefig(save_path)
-
-    plt.show()
-
-    print("\n", "Minimum Validation Loss: {:0.4f}".format(history_df['val_loss'].min()), "\n")
-    print("Corresponding Validation Accuracy: {:0.4f}".format(history_df['val_accuracy'][history_df['val_loss'].idxmin()]), "\n")
-
-def example_data_loader(img_size=(224, 224)):
+def example_data_loader():
     # Assuming typical input is a batch of images from the data loader
-    train_loader, _ = create_data_generators(DATAPATH, img_size)
-    example_input, _ = next(iter(train_loader))
+    train_loader, _ = create_data_generators(DATAPATH, val_path=None, batch_size=BATCH_SIZE, val_split=0)
+    example_input = next(iter(train_loader))[0]
     example_input = example_input.to(device)
     example_output = model(example_input)
 
@@ -114,21 +120,16 @@ def example_data_loader(img_size=(224, 224)):
 
 def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, callbacks, device):
     """Train the model."""
-    best_model = model
-    # Log model parameters (e.g., hyperparameters)
-    mlflow.log_param("learning_rate", optimizer.defaults['lr'])
-    mlflow.log_param("batch_size", train_loader.batch_size)
-    mlflow.log_param("epochs", epochs)
-    mlflow.log_param("patience", early_stopping['patience'])
-    # Set some tags for metadata
-    
-    mlflow.set_tag("model_type", "EfficientNetB0")
-    #mlflow.set_tag("note", "First run using MLflow")
+    best_model = model.to(device)
 
     total_training_time = 0  # Track total training time for all epochs
     total_steps = 0  # Track total steps across all epochs
 
-    history = {'loss': [], 'val_loss': [], 'accuracy': [], 'val_accuracy': []}
+    # Log model parameters (e.g., hyperparameters)
+    mlflow.log_param("batch_size", train_loader.batch_size)
+    mlflow.log_param("epochs", EPOCHS)
+
+    scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(epochs):
 
@@ -141,10 +142,14 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, c
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+
+            with torch.cuda.amp.autocast(): # Mixed precision training
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+            scaler.scale(loss).backward() 
+            scaler.step(optimizer)
+            scaler.update()
             
             running_loss += loss.item() * inputs.size(0)
             _, predicted = torch.max(outputs, 1)
@@ -155,6 +160,9 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, c
         
         epoch_loss = running_loss / len(train_loader.dataset)
         epoch_acc = correct / total
+
+        scheduler1.step()
+        scheduler2.step(epoch_loss)
         
         #### MODEL EVALUATION ####
         model.eval()
@@ -201,16 +209,13 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, c
         mlflow.log_metric("train_accuracy", epoch_acc, step=epoch)
         mlflow.log_metric("val_loss", val_loss, step=epoch)
         mlflow.log_metric("val_accuracy", val_acc, step=epoch)
+        mlflow.log_metric("learning_rate", optimizer.param_groups[0]['lr'], step=epoch)
 
-        history['loss'].append(epoch_loss)
-        history['val_loss'].append(val_loss)
-        history['accuracy'].append(epoch_acc)
-        history['val_accuracy'].append(val_acc)
 
-        print(f'Epoch {epoch+1}/{epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_acc:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.4f}')
+        print(f'Epoch {epoch}/{epochs} : Loss: {epoch_loss:.4f}, Accuracy: {epoch_acc:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.4f}')
         
         # Early stopping
-        if callbacks['best_loss'] is None or val_loss < callbacks['best_loss'] - callbacks['min_delta']:
+        if callbacks['best_loss'] is None or val_loss < callbacks['best_loss']:
             callbacks['best_loss'] = val_loss
             callbacks['counter'] = 0
             best_model = model
@@ -221,21 +226,9 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, c
             if callbacks['counter'] >= callbacks['patience']:
                 callbacks['early_stop'] = True
                 callbacks['counter'] = 0
+                model = best_model # Load the best model found during training
                 print("Early stopping")
                 break
-
-   
-    # Log the model to MLflow at the end of training
-    torch.save(best_model.state_dict(), callbacks['checkpoint_path'])
-
-     # example_input, example_output = example_data_loader()
-    # signature = infer_signature(example_input.cpu().numpy(), example_output.cpu().detach().numpy())
-
-    # mlflow.pytorch.log_model(best_model, 
-    #                     artifact_path="model",
-    #                     signature=signature,
-    #                     registered_model_name="Classification-row-images" 
-    #                     )
 
 
     # Log validation metrics as tags
@@ -245,17 +238,27 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, epochs, c
     # Log the total average training time per step for all epochs
     overall_avg_time_per_step = total_training_time / total_steps
     mlflow.log_metric("overall_avg_time_per_step", overall_avg_time_per_step)
-    
 
-    return history
+def retrieve_metrics(metrics_to_retrieve) :
+    """Retrieve metrics from the MLflow run."""
+    client = mlflow.tracking.MlflowClient()
+    run_info = mlflow.active_run().info
+    run_id = run_info.run_id
 
+    # Collect run parameters and metrics
+    for metric in metrics_to_retrieve:
+        metric_history = client.get_metric_history(run_id, metric)
+        metrics_log[metric].extend(metric_history) #Append to previous history
+
+    steps = max(len(records) for records in metrics_log.values())
+
+    return metrics_log, steps 
 
 if __name__ == "__main__":
     print("\n","\n","Classifier training script", "\n")
 
     # Setup
-    set_seed()
-    setup_matplotlib()
+    #set_seed()
  
     # Create the graphs folder if it doesn't exist
     graphs_folder = 'graphs'
@@ -266,60 +269,78 @@ if __name__ == "__main__":
     IMG_SIZE = (224, 224)
     BATCH_SIZE = 20
     EPOCHS = 30
-    DATAPATH = 'datasets/classification_balanced'
+    PATIENCE = 10
+    DATAPATH = 'datasets/Luchey_balanced'
     SAVEPATH = 'models'
+    VAL_PATH = None
+    model_path = None
+    new_model_name = 'dev'
+    experiment = 'Classification-row-images-dev'
+    model_type = 'EfficientNetB0-optihead'
+
+    print(f'Run parameters: \n - Batch size: {BATCH_SIZE} \n - Epochs: {EPOCHS} \n - Patience: {get_callbacks(SAVEPATH)["patience"]} \n - Data path: {DATAPATH} \n - Save path: {SAVEPATH} \n - Validation set: {VAL_PATH != None} \n - Pretrained Model: {model_path != None} \n')
 
     class_names = sorted([d.name for d in os.scandir(DATAPATH) if d.is_dir()])
     
     # Create data generators
-    train_loader, val_loader = create_data_generators(DATAPATH, IMG_SIZE, BATCH_SIZE)
+    train_loader, val_loader = create_data_generators(DATAPATH, VAL_PATH, BATCH_SIZE)
+
 
     # Create and compile the model
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = create_model(IMG_SIZE).to(device)
+    print("Device:", device, "\n")
+    model = create_model(model_path).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler1 = ExponentialLR(optimizer, gamma=0.95)
+    scheduler2 = ReduceLROnPlateau(optimizer, mode='min', factor=0.60, patience=PATIENCE//2)
 
-    best_model = model
+    best_model = model.to(device)
 
-    mlflow.set_experiment('Classification-row-images')
+    mlflow.set_experiment(experiment)
     run_dt = f"Run_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-    print("Training...", "\n")
+    metrics_to_retrieve = [ "val_loss", 
+                            "val_accuracy",
+                            "learning_rate", 
+                            "train_loss", 
+                            "train_accuracy", 
+                            "overall_avg_time_per_step",
+                            "val_recall_missing_vine",
+                            "val_recall_turn",
+                            "val_recall_vine"
+                            ]
+    
+    # Initialize a dictionary to store the metrics
+    metrics_log = {metric: [] for metric in metrics_to_retrieve}
+
+    steps = 0
+
 
     with mlflow.start_run(run_name=f"{run_dt}") as run:
+        
+        run_name = run_dt
 
-        run_name = f"{run_dt}-0.001"  
+        # Log model parameters (e.g., hyperparameters)
+        mlflow.log_param("batch_size", train_loader.batch_size)
+        mlflow.log_param("epochs", EPOCHS)
+        mlflow.log_param("patience", get_callbacks(PATIENCE)['patience'])
+        mlflow.log_param("dataset", DATAPATH)
+
+
+        # Set some tags for metadata
+        mlflow.set_tag("model_type", model_type)
+        #mlflow.set_tag("note", "First run using MLflow")
 
         # Define early stopping callback
-        early_stopping, checkpoint_path = get_callbacks(SAVEPATH)
-        early_stopping['checkpoint_path'] = checkpoint_path
+        early_stopping = get_callbacks(PATIENCE)
+    
+        # Train the model
+        train_model(model, train_loader, val_loader, criterion, optimizer, EPOCHS, early_stopping, device)
+        metrics_log, steps = retrieve_metrics(metrics_to_retrieve)
         
-        with mlflow.start_run(run_name=f"{run_name}", nested = True) as run:
-
-            # Train the model
-            history = train_model(model, train_loader, val_loader, criterion, optimizer, EPOCHS, early_stopping, device)
-
-        history_df = pd.DataFrame(history).fillna(np.nan)
-        plot_history(history_df, save=False)
-
-        # Recompile the model with a lower learning rate
-        optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-        # Define early stopping callback for fine-tuning
-        early_stopping['min_delta'] = 0.0001
-        early_stopping['counter'] = 0
-        early_stopping['early_stop'] = False
-
-        run_name = f"{run_dt}-0.0001"
-
-        with mlflow.start_run(run_name=f"{run_name}", nested = True) as run:
-
-            print("\n","Continuing with lower learning rate...", "\n")
-            history = train_model(model, train_loader, val_loader, criterion, optimizer, EPOCHS, early_stopping, device)
-
-        history_df = pd.concat([history_df, pd.DataFrame(history).fillna(np.nan)], ignore_index=True)
-        plot_history(history_df, save=False)
+        # history_df = pd.DataFrame(history).fillna(np.nan)
+        # plot_history(history_df, save=False)
 
         print("\n", "NOW FINE-TUNING THE MODEL", "\n")
 
@@ -329,14 +350,41 @@ if __name__ == "__main__":
 
         # Recompile the model with a lower learning rate
         optimizer = optim.Adam(model.parameters(), lr=0.0001)
+        scheduler1 = ExponentialLR(optimizer, gamma=0.99)
+        scheduler2 = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=PATIENCE//2)
 
         run_name = f"{run_dt}-finetuning"
+
         with mlflow.start_run(run_name=f"{run_name}", nested = True) as run:
+            # Train the model
+            train_model(model, train_loader, val_loader, criterion, optimizer, EPOCHS, early_stopping, device)
+            metrics_log, steps = retrieve_metrics(metrics_to_retrieve)
 
-            # Continue training the model
-            history = train_model(model, train_loader, val_loader, criterion, optimizer, EPOCHS, early_stopping, device)
-            
-    print('Training completed. Model saved to:', SAVEPATH, '\n')    
-    history_df = pd.concat([history_df, pd.DataFrame(history).fillna(np.nan)], ignore_index=True)
+        # Save the model
+        torch.save(model.state_dict(), os.path.join(SAVEPATH, f'{run_dt}.pth'))
 
-    plot_history(history_df, save=True)
+        # Log the model to MLflow
+        example_input, example_output = example_data_loader()
+        signature = infer_signature(example_input.cpu().numpy(), example_output.cpu().detach().numpy())
+
+        mlflow.pytorch.log_model(pytorch_model = best_model, 
+                            artifact_path="model",
+                            signature=signature,
+                            registered_model_name=new_model_name
+                            )
+
+        print('Training completed. Model saved to:', SAVEPATH, '\n')    
+
+        # Log the metrics and parameters to the main run
+        for step in range(steps):
+            for metric in metrics_to_retrieve:
+                if step < len(metrics_log[metric]):
+                    mlflow.log_metric(metric, metrics_log[metric][step].value, step=step)
+
+        # Find the index at which val_accuracy was the highest
+        max_val_accuracy_index = max(range(len(metrics_log['val_accuracy'])), key=lambda i: metrics_log['val_accuracy'][i].value)
+
+        # Log the metrics for the index at which val_accuracy was the highest
+        for metric in metrics_to_retrieve:
+            if max_val_accuracy_index < len(metrics_log[metric]):
+                mlflow.log_metric(metric, metrics_log[metric][max_val_accuracy_index].value, step=steps+1)
